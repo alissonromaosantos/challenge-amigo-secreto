bb07ca5b0269d2adc97f83cd6c9aa70a8e8d23ed
Alisson Romão Santos <alissonromaosantos94@gmail.com>
2025-07-29 16:56:26 -0300

    Criadas as funcionalidades de adicionar um amigo e sortear um amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigoSecreto = document.querySelector("#amigo");
+let listaAmigos = document.querySelector("#listaAmigos");
+let resultado = document.querySelector("#resultado");
+const amigos = [];
+
+function criarTagLiAdicionarNaUl(tag, texto) {
+  let campo = document.createElement("li");
+  campo.innerHTML = texto;
+  campo.style.textAlign = "center";
+  tag.appendChild(campo);
+}
+
+function adicionarAmigo() {
+  criarTagLiAdicionarNaUl(listaAmigos, amigoSecreto.value);
+  amigos.push(amigoSecreto.value);
+  amigoSecreto.value = "";
+}
+
+function sortearAmigo() {
+  let sorteado = parseInt(Math.random() * amigos.length);
+  let amigoSorteado = amigos[sorteado];
+  criarTagLiAdicionarNaUl(resultado, `O amigo secreto sorteado é: ${amigoSorteado}!`);
+}
